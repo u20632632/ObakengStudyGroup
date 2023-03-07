abeb30d283dcc2960ef6f1a6c980a213508567a3
u20632632 <102987611+u20632632@users.noreply.github.com>
2023-03-07 23:36:39 +0200

    Add project files.

---
+++ ObakengStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObakengStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult ObakengStudyGroupView()
+        {
+            List<Models.PeopleClass> people = new List<Models.PeopleClass>();
+
+            people.Add(new Models.PeopleClass { StudentNumber = "u12345678", Name = "Jane", Surname = "Doe", EmailAddress = "jane.Doe@tuks.co.za" });
+            people.Add(new Models.PeopleClass { StudentNumber = "u01234567", Name = "John", Surname = "Darn", EmailAddress = "john.Darn@tuks.co.za" });
+            people.Add(new Models.PeopleClass { StudentNumber = "u11234567", Name = "Terrence", Surname = "straught", EmailAddress = "Terrence.straught@tuks.co.za" });
+            people.Add(new Models.PeopleClass { StudentNumber = "u12234567", Name = "Angel", Surname = "Shileyi", EmailAddress = "Angel.Shileyi@tuks.co.za" });
+            people.Add(new Models.PeopleClass { StudentNumber = "u12345677", Name = "Mike", Surname = "Vactor", EmailAddress = "Mike.Vactor@tuks.co.za" });
+
+
+            return View(people);
+
+        }
+    }
+}
---
+++ ObakengStudyGroup/Models/PeopleClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; // Add to project
+
+namespace ObakengStudyGroup.Models
+{
+    public class PeopleClass
+    {
+        [Display(Name = "STUDENT NUMBER")] //Add as decorator
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")] //Add as decorator
+        public string Name { get; set; }
+
+        [Display(Name = "SURNAME ")] //Add as decorator
+        public string Surname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")] //Add as decorator
+        public string EmailAddress { get; set; }
+      
+
+    }
+}
